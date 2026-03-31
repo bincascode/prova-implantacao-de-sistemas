bee318f5a0a32cce325ea780edab5df5872efe31
BIANCA DA SILVA FERREIRA <232.976230@universidadesenaicimatec.edu.br>
2026-03-31 15:20:08 -0300

    conexão com supabse

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const { createClient } = require('@supabase/supabase-js');
+require('dotenv').config();
 app.use(express.static('public'));
 const PORT = 4000;
+
+
+
+// Pegando as variáveis de ambiente
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+// Criando o cliente de conexão
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Rota para servir a página HTML
 app.get('/', (req, res) => {
